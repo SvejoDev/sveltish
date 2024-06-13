d087b49211664acb462b5a2b4221270af99d7a1d
johan <johan.svensson@gtsmarketing.se>
2024-06-13 21:47:29 +0200

    userStore function

--- myApp/src/lib/firebase.ts
+++ myApp/src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, setPersistence, browserSessionPersistence } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 // Konfigurationen laddas från miljövariabler
 const firebaseConfig = {
@@ -14,21 +15,25 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 
-// Sätt uthållighet för autentisering
-setPersistence(auth, browserSessionPersistence).catch(console.error);
 
-// Logga miljövariabler för felsökning
-console.log("Firebase Config:", {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-});
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+  const currentUser = writable<User | null>(null);
+
+  onAuthStateChanged(auth, (user) => {
+    currentUser.set(user);
+
+  });
+}
+
+export const user = userStore();
+
+
+
