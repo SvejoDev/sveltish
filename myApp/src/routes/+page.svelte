7cf9f2db429285cf0a6c24dcb953ab17055d5856
johan <johan.svensson@gtsmarketing.se>
2024-06-13 15:00:29 +0200

    added google sign in

--- myApp/src/routes/+page.svelte
+++ myApp/src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1 class=" text-green-400">Welcome to SvelteKit</h1>
+<div class="flex justify-center my-6 flex-col mx-115">
+    <h1 class=" text-green-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button class="btn">
     <h2>Hello</h2>
 </button>
+</div>
