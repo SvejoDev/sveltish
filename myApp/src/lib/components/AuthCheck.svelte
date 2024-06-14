d8e52b5d36d524f2e63cfc76ed60bce1c93478cf
johan <johan.svensson@gtsmarketing.se>
2024-06-14 15:34:52 +0200

    store+backend

---
+++ myApp/src/lib/components/AuthCheck.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { user } from "$lib/firebase";
+</script>
+  
+{#if $user}
+  <slot />
+{:else}
+<div class="container flex flex-col self-center max-w-52">
+    <p class="text-error align-middle">
+        You must be signed in to view this page.
+        
+    </p>
+    <a class="btn btn-primary" href="/login">Sign in</a>
+</div>
+    
+{/if}
--- myApp/src/lib/firebase.ts
+++ myApp/src/lib/firebase.ts
@@ -4,7 +4,7 @@ import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { writable } from "svelte/store";
 
-// Konfigurationen laddas från miljövariabler
+// Firebase-konfigurationen laddas från miljövariabler
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -15,25 +15,45 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+// Initialisera Firebase-applikationen med konfigurationen
 export const app = initializeApp(firebaseConfig);
+// Hämta Firestore-databasen
 export const db = getFirestore(app);
+// Hämta Firebase-autentisering
 export const auth = getAuth(app);
+// Hämta Firebase-lagring
 export const storage = getStorage(app);
 
-
 /**
- * @returns a store with the current firebase user
+ * Skapar en Svelte store för att hantera aktuell inloggad Firebase-användare
+ * @returns en store med den aktuella Firebase-användaren
  */
 function userStore() {
-  const currentUser = writable<User | null>(null);
+  let unsubscribe: () => void;
 
-  onAuthStateChanged(auth, (user) => {
-    currentUser.set(user);
+  // Kontrollera om auth är initialiserad och om vi är i en browsermiljö
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    };
+  }
 
+  // Skapa en writable store med den aktuella användaren (eller null om ingen är inloggad)
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    // Sätt upp en lyssnare för autentiseringsändringar
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user); // Uppdatera store när användaren ändras
+    });
+
+    return () => unsubscribe(); // Rensa lyssnaren när storen inte används längre
   });
+
+  return {
+    subscribe,
+  };
 }
 
+// Exportera user storen för att användas i andra delar av applikationen
 export const user = userStore();
-
-
-
